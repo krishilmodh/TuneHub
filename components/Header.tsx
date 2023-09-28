48081a7f53d89a52d1c8a4865f4fdd9ed8cf36e5
krishimodh <krishimodh@gmail.com>
2023-09-28 14:32:36 +0530

    style: :lipstick: add button
    update Header commponent
    
    add home and search button

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -16,45 +18,91 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 
   return (
     <div
-      className={twMerge(`
-  h-fit
-  bg-gradient-to-b
-  from-emerald-800
-  p-6
-   `)}
+      className={twMerge(
+        `
+        h-fit
+        bg-gradient-to-b
+        from-emerald-800
+        p-6
+   `,
+        className
+      )}
     >
       <div
         className="
-        hidden
-        md:flex
-        gap-x-2
-        items-center
-        "
+      w-full
+      mb-4
+      flex
+      items-center
+      justify-between
+      "
       >
-        <button
+        <div
           className="
-          rounded-full
-          bg-black
-          flex
-          item-center
-          hover:opacity-75
-          transition
+            hidden
+            md:flex
+            gap-x-2
+            items-center
         "
         >
-          <RxCaretLeft size={35} />
-        </button>
-        <button
-          className="
-          rounded-full
-          bg-black
-          flex
-          item-center
-          hover:opacity-75
-          transition
-        "
-        >
-          <RxCaretRight size={35} />
-        </button>
+          <button
+            onClick={() => router.back()}
+            className="
+              rounded-full
+              bg-black
+              flex
+              item-center
+              hover:opacity-75
+              transition
+          "
+          >
+            <RxCaretLeft size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="
+              rounded-full
+              bg-black
+              flex
+              item-center
+              hover:opacity-75
+              transition
+          "
+          >
+            <RxCaretRight size={35} />
+          </button>
+        </div>
+
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            item-center
+            justify-center
+            hover:opacity-75
+            transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            item-center
+            justify-center
+            hover:opacity-75
+            transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
       </div>
     </div>
   );
