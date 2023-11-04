8b69c667da9abe80fedd3a73faeac7f97f30d1b4
krishimodh <krishimodh@gmail.com>
2023-11-04 21:38:15 +0530

    feat: :sparkles: Supabse connection
    supabase connection and database page created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
