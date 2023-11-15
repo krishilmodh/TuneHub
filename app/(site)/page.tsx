7b4a651247c584061919f3ede6ef6bcee0030c77
krishimodh <krishimodh@gmail.com>
2023-11-15 23:49:41 +0530

    Songs fetching and list display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,12 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
+export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div
       className="
@@ -44,8 +48,11 @@ export default function Home() {
           </div>
         </div>
       </Header>
-      <div className="">
-        
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
---
+++ components/SongItem.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+import Image from "next/image";
+
+interface SongItemProps {
+  data: Song;
+  onClick: (id: string) => void;
+}
+
+const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoadImage(data);
+  return (
+    <div
+      onClick={() => onClick(data.id)}
+      className="
+        relative
+        group 
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-400/5
+        cursor-pointer
+        hover:bg-neutral-400/10
+        transition
+        p-3
+      "
+    >
+      <div
+        className="
+          relative
+          aspect-square
+          w-full
+          h-full
+          rounded-md
+          overflow-hidden
+        "
+      >
+        <Image
+          className="obeject-cover"
+          src={imagePath || '/images/liked.png'}
+          fill
+          alt="Image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SongItem;
