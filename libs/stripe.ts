57b45e088ffc57d93faa13f292bc423a9bfeb5a1
krishimodh <krishimodh@gmail.com>
2024-01-08 15:31:56 +0530

    feat: :sparkles: Stripe Integration
    Stripe Integration
    
    Stripe Integration

---
+++ libs/stripe.ts
@@ -0,0 +1,24 @@
+// import Stripe from "stripe";
+
+// export const stripe = new Stripe (
+//     process.env.STRIPE_SECRET_KEY ?? '',
+//     {
+//         apiVersion:"2023-10-16",
+//         appInfo:{
+//             name:'Spotify Clone Video',
+//             version:'0.1.0'
+//         }
+//     }
+// )
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY ?? '',
+  {
+    apiVersion: '2023-10-16',
+    appInfo: {
+      name: 'Spotify Clone',
+      version: '0.1.0'
+    }
+  }
+);
